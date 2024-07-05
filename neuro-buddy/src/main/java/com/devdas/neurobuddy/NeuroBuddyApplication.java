21d941121cc3352624a3b755d8682884d3f5a467
lokesh291410 <lokesh.work2910gmail.com>
2024-07-05 16:01:35 +0530

    created backend service

---
+++ neuro-buddy/src/main/java/com/devdas/neurobuddy/NeuroBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.devdas.neurobuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NeuroBuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NeuroBuddyApplication.class, args);
+    }
+
+}
---
+++ neuro-buddy/src/test/java/com/devdas/neurobuddy/NeuroBuddyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devdas.neurobuddy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NeuroBuddyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
