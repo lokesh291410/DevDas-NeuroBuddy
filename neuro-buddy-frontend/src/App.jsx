cf814de6e6213019ce24a4093c31d85ee7b780c9
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-07-06 13:00:30 +0530

    hcthn day1

--- neuro-buddy-frontend/src/App.jsx
+++ neuro-buddy-frontend/src/App.jsx
@@ -1,7 +1,6 @@
 import "./App.css";
 import ChatUI from "./components/ChatUI.jsx";
-import Sidebar from "./components/Sidebar.jsx";
-import ChatArea from "./components/ChatArea.jsx";
+
 function App() {
   return (
     <>
