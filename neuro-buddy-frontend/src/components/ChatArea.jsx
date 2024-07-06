1733cfa0372c13398aba09d0e9cfffbb0c95ad68
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-07-06 19:14:03 +0530

    Chat user Side1

--- neuro-buddy-frontend/src/components/ChatArea.jsx
+++ neuro-buddy-frontend/src/components/ChatArea.jsx
@@ -1,25 +1,79 @@
 import React from "react";
+import { useState, useEffect, useRef } from "react";
 
 const ChatArea = () => {
+  const [messages, setMessages] = useState([]);
+  const [inputValue, setInputValue] = useState("");
+  const messagesEndRef = useRef(null);
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: "smooth" });
+  };
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [messages]);
+
+  const handleInputChange = (e) => {
+    setInputValue(e.target.value);
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    if (!inputValue.trim()) {
+      return;
+    }
+    const userMessage = inputValue;
+    setMessages((prevMessages) => [
+      ...prevMessages,
+      { role: "user", message: userMessage },
+    ]);
+    setInputValue("");
+    //AI response
+  };
+
   return (
     <>
       <div
-        className="w-3/4 mb-4 mr-5 ml-2 p-4 rounded-xl relative"
+        className="flex flex-col w-3/4 mb-4 mr-5 ml-2 p-4 rounded-xl relative"
         style={{ height: "86vh" }}
       >
-        <div className="w-full absolute bottom-0 right-0 flex justify-center items-center pb-1 gap-2">
+        <div
+          className="flex flex-col items-end overflow-auto "
+          style={{ flexGrow: 1 }}
+        >
+          {messages.map((msg, index) => (
+            <div
+              key={index}
+              className={`p-2 rounded-lg m-2 ${
+                msg.sender === "User" ? "bg-blue-500" : "bg-green-500"
+              }`}
+              style={{ maxWidth: "50%", wordWrap: "break-word" }}
+            >
+              {msg.message}
+            </div>
+          ))}
+          <div ref={messagesEndRef} />
+        </div>
+
+        <form
+          onSubmit={handleSubmit}
+          className="w-full flex justify-center items-center pb-1 gap-2"
+        >
           <input
             type="text"
             placeholder="Type your message..."
+            value={inputValue}
+            onChange={handleInputChange}
             className="p-2 rounded-lg flex-grow outline-none border-2 border-gray-500 hover:border-white bg-gray-800 text-white"
           />
           <button
             type="submit"
-            className="bg-gray-800 px-4 py-2  rounded-lg text-white font-bold hover:bg-gray-700 active:bg-green-950"
+            className="bg-gray-800 px-4 py-2 rounded-lg text-white font-bold hover:bg-gray-700 active:bg-green-950"
           >
             Submit
           </button>
-        </div>
+        </form>
       </div>
     </>
   );
