45174f160380d59a1d3dd7dd73d29df83e586435
Ankit Kumar Singh <160294709+AskitEndo@users.noreply.github.com>
2024-07-06 14:34:32 +0530

    sidebar 2

--- neuro-buddy-frontend/src/components/Sidebar.jsx
+++ neuro-buddy-frontend/src/components/Sidebar.jsx
@@ -1,13 +1,28 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import maleImage from "../assets/male.png";
+import femaleImage from "../assets/female.png";
+import docData from "../../../DocData.json";
 
 function Sidebar() {
+  const [gender, setGender] = useState("");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * docData.length);
+    const selectedGender = docData[randomIndex].gender;
+    setGender(selectedGender);
+  }, []);
+
   return (
     <>
       <div
-        className="w-1/4 mb-4 ml-5 mr-2 border border-slate-400 p-4 rounded-xl"
+        className=" flex flex-col w-1/4 mb-4 ml-5 mr-2 border border-slate-400 p-4 rounded-xl justify-start items-center"
         style={{ height: "86vh" }}
       >
-        Sidebar
+        <img
+          src={gender === "Male" ? maleImage : femaleImage}
+          alt={gender}
+          className="w-1/2"
+        />
       </div>
     </>
   );
